febd12ab1631152b4c48958431bbe68b59f7f61f
solo <devendrarawat76@gmail.com>
2023-04-07 12:06:11 +0200

    Added base url for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import styles from "./App.module.scss"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import { Session } from "./Routes/Session/Session"
-import { Join } from "./Routes/Join/Join"
-import { Home } from "./Routes/Home/Home"
-import { WaitingArea } from "./Routes/Waiting/Waiting"
-import { initializePeerConnection } from "./Connection/Connection"
+import styles from "./App.module.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Session } from "./Routes/Session/Session";
+import { Join } from "./Routes/Join/Join";
+import { Home } from "./Routes/Home/Home";
+import { WaitingArea } from "./Routes/Waiting/Waiting";
+import { initializePeerConnection } from "./Connection/Connection";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Home,
+    },
+    {
+      path: "/session",
+      Component: Session,
+    },
+    {
+      path: "/join",
+      Component: Join,
+    },
+    {
+      path: "/wait",
+      Component: WaitingArea,
+    },
+  ],
   {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "/session",
-    Component: Session,
-
-  },
-  {
-    path: "/join",
-    Component: Join
-
-  },
-  {
-    path: "/wait",
-    Component: WaitingArea
+    basename: import.meta.env.BASE_URL,
   }
-])
+);
 function App() {
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
+initializePeerConnection();
 
-
-initializePeerConnection()
-
-export default App
-
+export default App;
